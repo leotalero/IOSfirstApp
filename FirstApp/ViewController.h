c5c42b070953bfcc2cebed8485d563fd39c421fb
leotalero <leo.20waw@hotmail.com>
2015-07-20 16:40:22 -0500

    Initial Commit

---
+++ FirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstApp
+//
+//  Created by Leonardo Talero on 7/20/15.
+//  Copyright (c) 2015 unir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FirstApp
+//
+//  Created by Leonardo Talero on 7/20/15.
+//  Copyright (c) 2015 unir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
