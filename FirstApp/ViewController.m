c62462e8f19ad1ee63e830d7e4420c2d425da034
leotalero <leo.20waw@hotmail.com>
2015-07-20 20:35:45 -0500

    first commit

--- FirstApp/ViewController.m
+++ FirstApp/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
 
 @end
@@ -17,6 +18,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSURL *prima_url=[NSURL URLWithString:
+                      @"http://www.eleconomista.es/prima-riesgo/espana"                       ];
+    NSURLRequest *request =[NSURLRequest requestWithURL:prima_url cachePolicy:0 timeoutInterval:5.0];
+    NSData *data=[NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSString *page_content=[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] ;
+    NSRegularExpression *regex=[NSRegularExpression regularExpressionWithPattern:@"\\| (\\d{3},\\d{2})" options:0 error:NULL];
+    NSTextCheckingResult *match=[regex firstMatchInString:page_content options:0 range:NSMakeRange(0, [page_content length])];
+    NSString *primastring=[page_content substringWithRange:[match rangeAtIndex:1]];
+    self.primaLabel.text=primastring;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
